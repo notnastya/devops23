81be9566f7dd2cf8c56adc1e0002c563718a834e
Asya <81755331+notnastya@users.noreply.github.com>
2023-06-11 17:40:59 +0300

    Add files via upload

---
+++ pd_test.py
@@ -0,0 +1,10 @@
+# importing pandas library 
+import pandas as pd 
+ 
+# string values in the list  
+lst = ['Java', 'Python', 'C', 'C++', 
+         'JavaScript', 'Swift', 'Go'] 
+ 
+# Calling DataFrame constructor on list 
+dframe = pd.DataFrame(lst) 
+print(dframe) 
