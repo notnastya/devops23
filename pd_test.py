62bd5790cf3b7fc67aa7bdf6f2159dfc36e55a40
Asya <81755331+notnastya@users.noreply.github.com>
2023-06-11 17:54:14 +0300

    Update pd_test.py

--- pd_test.py
+++ pd_test.py
@@ -3,7 +3,7 @@ import pandas as pd
  
 # string values in the list  
 lst = ['Java', 'Python', 'C', 'C++', 
-         'JavaScript', 'Swift', 'Go', 'R'] 
+         'JavaScript', 'Swift', 'Go', 'R', 'PASCAL'] 
  
 # Calling DataFrame constructor on list 
 dframe = pd.DataFrame(lst) 
